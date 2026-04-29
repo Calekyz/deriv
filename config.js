c9b1d6ef11d734e1f3e25a718eab049e96272e42
Mentor Calekyz <caleborenge8@gmail.com>
2026-04-30 01:42:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,10 @@
 
 const APP_ID = "3301r2nWfUV4FzUj536sd";
 
-// For demo trading (recommended to start)
-const USE_DEMO = true;
-
 // WebSocket endpoint
 const WS_URL = `wss://ws.derivws.com/websockets/v3?app_id=${APP_ID}`;
 
-// Export for use in script.js
+// Export for use in other files
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { APP_ID, USE_DEMO, WS_URL };
+    module.exports = { APP_ID, WS_URL };
 }
